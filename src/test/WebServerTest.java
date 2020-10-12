f407cd4fa6b59843c863bc7966d0d71e7aa30d15
stefaniabudau <stefania_budau@yahoo.com>
2020-10-12 21:42:46 +0300

    Uploaded WebServer project structure

---
+++ src/test/WebServerTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebServerTest {
+
+    @Test
+    public void test1(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("yay");
+    }
+}
